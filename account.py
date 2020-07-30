1f12491a6671b1271e1dff4c1912c1555296a2e1
student <kamauesthermuthoni@gmail.com>
2020-07-30 12:36:09 +0300

    my second push of my bank account work

--- account.py
+++ account.py
@@ -6,26 +6,60 @@ class BankAccount:
         self.balance=0
         self.bank=bank
         self.phone_number=phone_number
+        self.deposit=[]
+        self.withdrawals=[]
+        self.loan=0
 
     def account_name(self):
         name="{} account for {} {}".format(self.bank,self.first_name,self.last_name)
         return name
 
-    def deposit_statement(self,amount):
-        self.balance+=amount
-        print("You have deposited {} to your account".format(amount))
+    def deposit(self,amount):
+        if amount<=0:
+           print("You cannot deposit zero or negative")
+        else:
+            self.balance+=amount
+            self.deposit.append(amount)  
+            print("You have deposited {} to {}".format(amount,self.account_name()))
 
+
+    def withdraw(self,amount):
+        if amount<=0:
+            print("You cannot withdraw zero or a negative")
+
+        elif amount > self.balance:
+            print("You dont have enough balance")
+        else:
+            self.balance==amount
+            self.withdrawals.append(amount)
+            print("You have withdrawn {} from {}".format(amount,self.accont_name()))    
+    
     def get_balance(self):
         return "The balance for {} is {}".format(self.accountName(),self.balance)
+    
+    def show_deposit_statement(self):
+        for deposit in self.deposits:
+            print(deposit)
 
-    def withdraw_statement(self,cash):
-        self.cash=cash
-        print("You have withdrawn {} amount from your account".format(self.cash))
+    def show_withdrawals_statement(self):
+        for withdrawal in self.withdrawals:
+            print(withdrawal)
 
-    def loan(self,loan_amount):
-         self.loan=loan_amount
-         print("You have been given a loan worth {}".format(loan_amount))
+      
+    def request_loan(self,amount):
+        if amount<=0:
+           print("The loan request is invalid")
+        else:
+            self.loan=amount
+            print("You have been given a loan worth {}".format(amount))
 
-    def repay_loan(self):
-        self.pay+=loan_amount
-        print("You have paid your loan of amount {}".format(loan_amount))
+    def repay_loan(self,amount): 
+        if amount<=0:
+            print("Can't repay that amount")
+        elif:
+            amount>self.loan
+            print("Your loan is {} please enter an amount less or equal to.".format(self.loan))
+
+        else:
+            self.loan=>amount
+            print("You have repaid your loan with {}.Your balance is {}".format(amount,self.loan))
